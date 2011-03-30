efbc90ef6bfe8b57578b9678b9f8f33cf2a8c295
Robert Brandin <tumest@gmail.com>
2011-03-30 11:49:23 -0600

    Finished logic, cleaned everything up a bit.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,22 +4,41 @@ class MainController < ApplicationController
     if params[:name].blank?
     else
       name = params[:name]
-      url = "http://twitter.com/users/show/" + name + ".json"
-      result = HTTParty.get(url)
-      if result.to_s.include? "Not found"
+      url = make_url(name)
+      user_info = grab_userpage(url)
+      if user_info.header.to_s.include? "HTTPNotFound"
         redirect_to root_path
       else
-        followers = result.fetch("followers_count")
-        following = result.fetch("friends_count")
-        if followers > 250000
-          fame_level = "you are a twitter superstar"
-        else
-          fame_level = "no"
-        end
-        @fame_level = fame_level
+        @fame_level = evaluate_fame(user_info)
       end
     end
   end
+  
+  private
+    
+    def make_url(name)
+      "http://twitter.com/users/show/" + name + ".json"
+    end
+    
+    def grab_userpage(url)
+      HTTParty.get(url)
+    end
+    
+    def evaluate_fame(user_info)
+      followers = user_info.fetch("followers_count")
+      following = user_info.fetch("friends_count")
+      if followers > 250000
+        "you are a twitter superstar"
+      elsif (followers > 10000) && (followers < 250000)
+        "yes"
+      elsif (followers - following).abs < (0.1*followers + 0.1*following).ceil
+        "you are breaking even"
+      elsif (following) > (10*followers)
+        "you are inversely twitter famous"
+      else
+        "no"
+      end
+    end
   
 end
 
--- spec/controllers/main_controller_spec.rb
+++ spec/controllers/main_controller_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe MainController do
+  render_views
+  
+  describe "GET 'home'" do
+    
+    it "should be successful" do
+      get :home
+      response.should be_success
+    end
+  end
+  
+  describe "GET 'home' with empty params" do
+    
+    
+    it "should have an input for a twitter username" do
+      get :home
+      response.should have_selector("input[name='name'][type='text']")
+    end
+  end
 
 end
