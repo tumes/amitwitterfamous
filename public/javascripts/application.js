e27e7764652681a574538ed187d4344abb057105
Robert Brandin <tumest@gmail.com>
2011-03-31 15:17:21 -0600

    Figured out how to prevent multiple submissions of the same name as well as blank names.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,20 +1,31 @@
 $(document).ready(function(){
+  
+  result_appender = function(fame_level){
+    $(fame_level).hide().appendTo('body').delay(600).fadeIn('slow', function() {
+      $('.footer').delay(500).fadeIn(1500)
+    });
+  };
 
-$('input#famebutton').live('click', function() {
+  var active_twitter_name = ""
+
+$('input#famebutton').click( function() {
   var twitter_name = $("input#fameinput").val();
-  $(".spacer").animate({ height: "50px"}, 600);
-  $.ajax({
-    dataType: "json",
-    cache: false,
-    url: '/' + twitter_name,
-    timeout: 2000,
-    success: function(data){
-      var fame_level = "<div class='container' id='padded'><h1>" + data + "</h1></div>"
-      $(fame_level).hide().appendTo('body').delay(600).fadeIn('slow', function() {
-        $('.footer').delay(500).fadeIn(1500)
-      });
+    if (twitter_name != active_twitter_name) {
+      $(".spacer").animate({ height: "50px"}, 600);
+        $.ajax({
+          dataType: "json",
+          cache: false,
+          url: '/' + twitter_name,
+          timeout: 2000,
+          success: function(data){
+            var fame_level = "<div class='container' id='padded'><h1>" + data + "</h1></div>"
+            result_appender(fame_level)
+            active_twitter_name = twitter_name
+          }
+        });
     }
+    else {
+    };  
+    return false
   });
-  return false
-});
 });
