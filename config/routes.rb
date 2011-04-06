6db8a911d7b530ad33f02f90fd73e6b1248bb796
Robert Brandin <tumest@gmail.com>
2011-04-05 21:58:37 -0600

    Removing javascript-free version of site and name routing feature. Not really necessary.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Amitwitterfamous::Application.routes.draw do
   root :to => "main#home"
   
-  match '/:name', :to => 'main#home'
+  # match '/:name', :to => 'main#home'
   match '/tellme/about', :to => 'main#about', :as => 'about'
 
 end
