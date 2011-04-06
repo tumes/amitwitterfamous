c9d2fb3b565a1ee3c272dd0718d22a1b5be5e5b0
Robert Brandin <tumest@gmail.com>
2011-04-05 23:35:00 -0600

    Heavy lifting down done with javascript on client side. Must remove old controller stuff.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
   
   result_appender = function(fame_level){
+    $(".container#padded").fadeOut(300);
     $(fame_level).hide().appendTo('body').delay(600).fadeIn('slow', function() {
       $('.footer').delay(200).fadeIn(900)
     });
@@ -11,18 +12,31 @@ $(document).ready(function(){
 $('input#famebutton').live('click', function() {
   var twitter_name = $("input#fameinput").val();
     if (twitter_name == "") {
-      result_appender("please enter a twitter username")
+      result_appender("<div class='container' id='padded'><h1>" + "please enter a twitter username" + "</h1></div>")
     }
     else if (twitter_name != active_twitter_name) {
-      $(".container#padded").fadeOut(300);
       $(".spacer").animate({ height: "50px"}, 600);
-        $.ajax({
-          dataType: "json",
-          cache: false,
-          url: '/' + twitter_name,
+      var url = 'http://twitter.com/users/show/' + twitter_name + '.json'
+        $.ajax(url, {
+          crossDomain: true,
+          dataType: "jsonp",
           timeout: 5000,
-          success: function(data){
-            var fame_level = "<div class='container' id='padded'><h1>" + data + "</h1></div>"
+          success: function(data, text, xhqr){
+            var followers = data.followers_count
+            var following = data.friends_count
+            var fame_result = ''
+            if (followers >= 250000) {
+              fame_result = 'you are a twitter superstar'
+            } else if (followers >= 10000 && followers < 250000) {
+              fame_result = 'yes'
+            } else if (followers >= 1000 && followers < 10000) {
+              fame_result = "you're getting there"
+            } else if (following > 10*followers) {
+              fame_result = "you're inversely twitter famous"
+            } else {
+              fame_result = 'no'
+            }
+            var fame_level = "<div class='container' id='padded'><h1>" + fame_result + "</h1></div>"
             result_appender(fame_level)
             active_twitter_name = twitter_name
           }
