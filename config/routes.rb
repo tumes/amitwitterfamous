6209ca7164c7f0113cb37c7ddb793e54ade78271
Robert Brandin <tumest@gmail.com>
2011-05-09 12:57:02 -0600

    Cleanup.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Amitwitterfamous::Application.routes.draw do
   root :to => "main#home"
   
-  # match '/:name', :to => 'main#home'
-  # match '/tellme/about', :to => 'main#about', :as => 'about'
-
 end
