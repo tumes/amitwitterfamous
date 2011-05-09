cd924d7905abd35f52f9913eec835d30f68a869b
Robert Brandin <tumest@gmail.com>
2011-05-09 15:25:39 -0600

    Added a wink for room 214

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -33,6 +33,8 @@ $('input#famebutton').live('click', function() {
               fame_result = 'you are a twitter superstar'
             } else if (followers >= 10000 && followers < 250000) {
               fame_result = 'yes'
+            } else if (sanitized_name == 'room_214') {
+              fame_result = "of course you are <br> (but a new hire wouldn't hurt)"
             } else if (followers >= 1000 && followers < 10000) {
               fame_result = "you're getting there"
             } else if (Math.abs(followers - following) < Math.ceil(0.1*followers + 0.1*following)) {
